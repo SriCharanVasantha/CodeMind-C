2593a3559ff3f25aebd668f8edcb45dda7303d5d
Sri Charan <166209881+SriCharanVasantha@users.noreply.github.com>
2024-04-06 14:35:42 +0530

    Add files via upload

---
+++ basic_ifcondition.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	if(a>=b)
+	printf("a is greater and equal to b\n");
+	if(a<=b)
+	printf("a is less than and equal to b\n");
+	if(a==b)
+	printf("a is equal to b\n");
+	if(a>b);
+	printf("a is greater than b\n");
+	if (a<b)
+	printf("a is less than b");
+	return 0;
+}
